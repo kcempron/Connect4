5be2f753cdc07dc783918f97b5df7f9bab254eef
Kaycem Code <kaycemcode@Kaycems-MBP.lan>
2022-08-08 10:38:18 -0700

    Added Basic Scaffolding and Statically Checked

---
+++ board.py
@@ -0,0 +1,21 @@
+import player
+
+# Provides the structure on which pieces are played within the constraints of the game.
+class Board:
+	def __init__(self) -> None:
+		return None
+
+	def create_board(self) -> None:
+		return None
+
+	def clear_board(self) -> None:
+		return None
+
+	def set_piece(self) -> None:
+		return None
+
+	def get_board(self) -> None:
+		return None
+
+	def view_board(self) -> None:
+		return None
